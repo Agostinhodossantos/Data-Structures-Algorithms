ce6738972b52b483abc576928e393a3dda2e913e
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-10-15 21:48:36 +0200

    array operations

--- data/array.c
+++ data/array.c
@@ -1,15 +1,55 @@
 #include <stdio.h>
 
-main() {
-    // Traverse Operation
-    // This operation is to traverse through the elements of an array.
-    int LA[] = {1, 3, 5, 7, 8};
-    int item = 10, k = 3, n= 5;
-    int i = 0, j = n;
-    printf("The original array elements are: \n");
+int LA[] = {1,3,5,7,8};
+int item = 10, k = 3, n = 5;
+int i = 0, j = 5;
 
-    for(i = 0; i < n; i++) {
-        printf("LA[%d] = %d \n", i, LA[i]);
+void add() {
+   n = n + 1;
+   while (j >= k) {
+       LA[j+1] = LA[j];
+       j = j - 1;
+   }
+
+   LA[k] = item;
+}
+
+
+void delete() {
+    j = k;
+
+    while ( j < n) {
+        LA[j - 1] = LA[j];
+        j = j + 1;
     }
- 
+
+    n = n - 1;
+    
+}
+
+int find(int item) {
+    while (j < n) {
+        if (LA[i] == item) {
+            break;
+        }
+
+        j = j + 1;
+    }
+    return LA[i];
+}
+
+void update() {
+    LA[k-1] = item;
+}
+
+main() {
+  add();
+  int f = find(2);
+  printf("FIND[%d]", f);
+
+//   for ( i = 0; i < n; i++)
+//   {
+//    printf("LA[%d] = %d \n", i, LA[i]);
+//   }
+  
 }
