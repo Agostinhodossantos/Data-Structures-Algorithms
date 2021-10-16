c93b81f5e1314ee32644c18605af8f2160dfe2d3
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-10-16 13:16:11 +0200

    add linkedList

---
+++ data/linkedList/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       LinkedList<String> al = new LinkedList<String>();
+       al.add("Ravi");
+       al.add("Agostinho");
+       al.add("Dos");
+       al.add("Santos");
+
+       // Adding an element at the specific position
+       al.add(1, "Gaurav");
+       
+       // Adding second list elements to the first list
+       al.addAll(al);
+
+       // Adding an elements at the first
+       al.addFirst("Lokesh");
+
+       // Adding an element at the last position
+       al.addLast("Harsh");
+
+       // Removing specific element from arraylist
+       al.remove("Santos");
+
+       // Removing all the elements from arrayList
+       //al.removeAll(al);
+
+       Iterator<String> itr = al.iterator();
+       while(itr.hasNext()) {
+        System.out.println(itr.next());
+       }
+    }
+}
