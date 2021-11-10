b8a203494765c8ba2570f62cf905ca4c7c8513e0
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-10 07:51:06 +0200

    write exercise

--- problem_solve/twosum.java
+++ problem_solve/twosum.java
@@ -1,3 +1,15 @@
+/*
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+*/
+
+
+
 class twosum {
     public static void main(String[] args) {
         int nums[] = {2,11,7,15}; 
@@ -20,16 +32,4 @@ class twosum {
 
     }
 
-    // public int[] twoSum(int[] nums, int target) {
-    //     int targetPosition[] = {0, 0};
-
-    //     for(int i = 0; i < nums.length; i++) {
-    //         for(int y = i; y < nums.length; y++) {
-    //             if(nums[i] == nums[i+1]) {
-    //                 System.out.println("Hello world");   
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
 }
