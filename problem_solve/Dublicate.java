f20dcd80bb5e52d9910307af44fc540e8453da9c
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-11 15:27:58 +0200

    find duplicate numbers using sort

--- problem_solve/Dublicate.java
+++ problem_solve/Dublicate.java
@@ -1,3 +1,7 @@
+/* 
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
 import java.util.HashSet;
 
 public class Dublicate{
@@ -17,4 +21,14 @@ public class Dublicate{
 
         return false;
     }
+
+    public static boolean containsDuplicate(long[] nums) {
+        Arrays.sort(nums);
+
+        for ( int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+
 }
