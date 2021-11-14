9a7717fb9cc958c7ee43e463e267cf296c271575
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-14 16:48:31 +0200

    implement binary search~

--- problem_solve/Dublicate.java
+++ problem_solve/Dublicate.java
@@ -11,6 +11,43 @@ public class Dublicate{
         System.out.println(result);
     }
 
+    private static int BinarySearchRecur(int x, int v[], int e, int d) {
+        int meio = (e + d) / 2;
+        if (v[meio] == x) {
+            return meio;
+        }
+
+        if (e >= d) {
+            return -1;
+        } else {
+            if (v[meio] < x) {
+                return BinarySearchRecur(x, v, meio + 1, d);
+            } else {
+                return BinarySearchRecur(x, v, e, meio - 1);
+            }
+        }
+    }
+
+     private static int BinarySearch(int vet[], int chave, int tam) {
+        int inf = 0;
+        int sup = tam - 1;
+        int meio;
+
+        while (inf <= sup) {
+            meio = (inf + sup) / 2;
+            if (chave == vet[meio]) {
+                return meio;
+            }
+            if (chave < vet[meio]) {
+                sup = meio;
+            } else {
+                inf = meio + 1;
+            }
+        }
+        return -1;
+    }
+
+
     public static boolean containsDuplicate(int[] nums) {
         HashSet<Integer> numbers = new HashSet<>();
 
